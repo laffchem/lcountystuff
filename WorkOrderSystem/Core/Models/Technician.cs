3d378ba90afe0270502882c57953d63a82d44a6c
John Laffey <laffeychem@gmail.com>
2024-02-25 20:59:09 -0500

    forgot to save technician entity

--- WorkOrderSystem/Core/Models/Technician.cs
+++ WorkOrderSystem/Core/Models/Technician.cs
@@ -2,7 +2,7 @@ namespace Models
 {
     public class Technician
     {
-        public required int TechnicianId { get; set; }
+        public required int id { get; set; }
         public required string FirstName { get; set; }
         public required string LastName { get; set; }
     
